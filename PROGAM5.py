eff45c426baa8a9465310f9a34baeb96a449e740
csgomezg0 <69282959+csgomezg0@users.noreply.github.com>
2022-11-13 13:03:24 -0500

    Add files via upload
    PROGRAM: Is a visualizator of images from a *.zip file, without open or extract files.  only read images with format .jpg, .png. in updates in time i created more options for this programs and an .exe for execute without necesity of python or other programing language.

---
+++ PROGAM5.py
@@ -0,0 +1,44 @@
+# Import the required libraries
+from tkinter import *
+from tkinter import ttk
+
+# Create an instance of tkinter frame or window
+win = Tk()
+
+# Set the size of the window
+win.geometry("700x350")
+
+running = True
+
+# Define a function to print the text in a loop
+def print_text():
+   if running:
+      print("Hello World")
+
+   win.after(1000, print_text)
+
+# Define a function to start the loop
+def on_start():
+   global running
+   running = True
+
+# Define a function to stop the loop
+def on_stop():
+   global running
+   running = False
+
+canvas = Canvas(win, bg="skyblue3", width=600, height=60)
+canvas.create_text(150, 10, text="Click the Start/Stop to execute the Code", font=('', 13))
+canvas.pack()
+
+# Add a Button to start/stop the loop
+start = ttk.Button(win, text="Start", command=on_start)
+start.pack(padx=10)
+
+stop = ttk.Button(win, text="Stop", command=on_stop)
+stop.pack(padx=10)
+
+# Run a function to print text in window
+win.after(1000, print_text)
+
+win.mainloop()
